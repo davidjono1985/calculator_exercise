66b1d9801f20210858ea299ad8c500ad78c848d0
David Johnson <david_jono@hotmail.com>
2019-11-09 14:44:02 +1000

    Added ability to add two values

--- exercise.rb
+++ exercise.rb
@@ -7,9 +7,17 @@ until quit do
     puts "4. Multiply"
     puts "5. Square"
     puts "q. Quit"
-
     user_input =  gets.chomp
     case user_input
+    when "1"
+        puts" what's yoru first number?"
+        num1 = gets.chomp.to_i
+        puts "What's your second number?"
+        num2 = gets.chomp.to_i
+        result = num1 + num2
+        puts result
+
+
     when "q"
     quit = true
     end
